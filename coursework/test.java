025d6e14d3ac491a2546f9167ae8289291840263
Brickerrr <80991933+Brickerrr@users.noreply.github.com>
2024-04-03 18:18:04 +0100

    Update test.java

--- coursework/test.java
+++ coursework/test.java
@@ -1,11 +1,12 @@
 import Character.*;
 import Regular_Enemies.*;
 import Game_Play.*;
-
+import Bosses.*;
 
 public class test {
     public static void main(String[] args) {
         Hero h1 = new Hero();
-        BossFight f1 = new BossFight(h1);
+        Boss b1 = new Bramzark();
+        BossFight f1 = new BossFight(h1, b1);
     }
 }
